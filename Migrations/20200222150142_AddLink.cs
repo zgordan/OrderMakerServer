d4511dcdf104cb3db26368d830066fa4bd1add02
Oleg Bruev <job4bruev@gmail.com>
2020-03-16 16:17:35 +0300

    Release 1.1.5.beta.2  Fix Error Migration

--- Migrations/20200222150142_AddLink.cs
+++ Migrations/20200222150142_AddLink.cs
@@ -6,12 +6,12 @@ namespace Mtd.OrderMaker.Server.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO neo_ordermaker.mtd_sys_type (id,name,description,active) values (13,'Link', 'Link',1)");
+            migrationBuilder.Sql("INSERT INTO mtd_sys_type (id,name,description,active) values (13,'Link', 'Link',1)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("DELETE FROM neo_ordermaker.mtd_sys_type where id=13");
+            migrationBuilder.Sql("DELETE FROM mtd_sys_type where id=13");
         }
     }
 }
